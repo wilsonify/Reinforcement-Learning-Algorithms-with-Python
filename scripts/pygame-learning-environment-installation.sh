cb03247b54ba0f2c63eb8c0d0a2e390f9b68be0b
wilsonify <tom.a.wilson@gmail.com>
2022-04-17 11:09:43 -0400

    save install instruction

---
+++ scripts/pygame-learning-environment-installation.sh
@@ -0,0 +1,21 @@
+
+# Installation
+
+# PLE requires the following libraries to be installed:
+# numpy
+# pillow
+# pygame
+
+# PyGame can be installed using this tutorial (Ubuntu). For mac you can use these following instructions;
+#brew install sdl sdl_ttf sdl_image sdl_mixer portmidi  # brew or use equivalent means
+#conda install -c https://conda.binstar.org/quasiben pygame  # using Anaconda
+
+# To install PLE first clone the repo:
+
+# git clone https://github.com/ntasfi/PyGame-Learning-Environment
+
+# Then use the cd command to enter the PyGame-Learning-Environment directory and run the command:
+
+sudo pip install -e .
+
+# This will install PLE as an editable library with pip.
