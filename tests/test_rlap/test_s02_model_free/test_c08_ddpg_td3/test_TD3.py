ae210b35e76c6f5ce600aa5ff2506e6ec81df830
wilsonify <tom.a.wilson@gmail.com>
2022-04-17 11:07:26 -0400

    better tests for S02

--- tests/test_rlap/test_s02_model_free/test_c08_ddpg_td3/test_TD3.py
+++ tests/test_rlap/test_s02_model_free/test_c08_ddpg_td3/test_TD3.py
@@ -2,164 +2,62 @@ import numpy as np
 import tensorflow as tf
 import gym
 from datetime import datetime
-from collections import deque
-import time
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+from rlap.s02_model_free.c08_ddpg_td3.TD3 import (
+    current_milli_time,
+    test_agent,
+    ExperiencedBuffer,
+    deterministic_actor_double_critic,
 
-
-def mlp(x, hidden_layers, output_layer, activation=tf.nn.relu, last_activation=None):
-    """
-    Multi-layer perceptron
-    """
-    for l in hidden_layers:
-        x = tf.layers.dense(x, units=l, activation=activation)
-    return tf.layers.dense(x, units=output_layer, activation=last_activation)
+)
 
 
-# CHANGED FROM DDPG!
-def deterministic_actor_double_critic(x, a, hidden_sizes, act_dim, max_act=1):
-    """
-    Deterministic Actor-Critic
-    """
-    # Actor
-    with tf.variable_scope("p_mlp"):
-        p_means = max_act * mlp(x, hidden_sizes, act_dim, last_activation=tf.tanh)
-
-    # First critic
-    with tf.variable_scope("q1_mlp"):
-        q1_d = mlp(
-            tf.concat([x, p_means], axis=-1), hidden_sizes, 1, last_activation=None
-        )
-
-    with tf.variable_scope(
-        "q1_mlp", reuse=True
-    ):  # Use the weights of the mlp just defined
-        q1_a = mlp(tf.concat([x, a], axis=-1), hidden_sizes, 1, last_activation=None)
-
-    # Second critic
-    with tf.variable_scope("q2_mlp"):
-        q2_d = mlp(
-            tf.concat([x, p_means], axis=-1), hidden_sizes, 1, last_activation=None
-        )
-    with tf.variable_scope("q2_mlp", reuse=True):
-        q2_a = mlp(tf.concat([x, a], axis=-1), hidden_sizes, 1, last_activation=None)
-
-    return (
-        p_means,
-        tf.squeeze(q1_d),
-        tf.squeeze(q1_a),
-        tf.squeeze(q2_d),
-        tf.squeeze(q2_a),
-    )
+def test_smoke():
+    print("fire?")
 
 
-class ExperiencedBuffer:
-    """
-    Experienced buffer
-    """
+def test_TD3():
+    env_name = "BipedalWalker-v2"
+    num_epochs = 2000
+    discount = 0.99
+    render_cycle = 10000
+    expl_noise = 0.1
 
-    def __init__(self, buffer_size):
-        # Contains up to 'buffer_size' experience
-        self.obs_buf = deque(maxlen=buffer_size)
-        self.rew_buf = deque(maxlen=buffer_size)
-        self.act_buf = deque(maxlen=buffer_size)
-        self.obs2_buf = deque(maxlen=buffer_size)
-        self.done_buf = deque(maxlen=buffer_size)
-
-    def add(self, obs, rew, act, obs2, done):
-        """
-        Add a new transition to the buffers
-        """
-        self.obs_buf.append(obs)
-        self.rew_buf.append(rew)
-        self.act_buf.append(act)
-        self.obs2_buf.append(obs2)
-        self.done_buf.append(done)
-
-    def sample_minibatch(self, batch_size):
-        """
-        Sample a mini-batch of size 'batch_size'
-        """
-        mb_indices = np.random.randint(len(self.obs_buf), size=batch_size)
-
-        mb_obs = [self.obs_buf[i] for i in mb_indices]
-        mb_rew = [self.rew_buf[i] for i in mb_indices]
-        mb_act = [self.act_buf[i] for i in mb_indices]
-        mb_obs2 = [self.obs2_buf[i] for i in mb_indices]
-        mb_done = [self.done_buf[i] for i in mb_indices]
-
-        return mb_obs, mb_rew, mb_act, mb_obs2, mb_done
-
-    def __len__(self):
-        return len(self.obs_buf)
-
-
-def test_agent(env_test, agent_op, num_games=10):
-    """
-    Test an agent 'agent_op', 'num_games' times
-    Return mean and std
-    """
-    games_r = []
-
-    for _ in range(num_games):
-        d = False
-        game_r = 0
-        o = env_test.reset()
-
-        while not d:
-            a_s = agent_op(o)
-            o, r, d, _ = env_test.step(a_s)
-
-            game_r += r
-
-        games_r.append(game_r)
-
-    return np.mean(games_r), np.std(games_r)
-
-
-def TD3(
-    env_name,
-    hidden_sizes=[32],
-    ac_lr=1e-2,
-    cr_lr=1e-2,
-    num_epochs=2000,
-    buffer_size=5000,
-    discount=0.99,
-    render_cycle=10000,
-    mean_summaries_steps=1000,
-    batch_size=128,
-    min_buffer_size=5000,
-    tau=0.005,
-    target_noise=0.2,
-    expl_noise=0.1,
-    policy_update_freq=2,
-):
+    hidden_sizes = [64, 64]
+    ac_lr = 4e-4
+    cr_lr = 4e-4
+    buffer_size = 200000,
+    mean_summaries_steps = 100,
+    batch_size = 64,
+    min_buffer_size = 10000,
+    tau = 0.005,
+    policy_update_freq = 2,
+    target_noise = 0.1,
 
     # Create an environment for training
     env = gym.make(env_name)
     # Create an environment for testing the actor
     env_test = gym.make(env_name)
 
-    tf.reset_default_graph()
+    tf.compat.v1.reset_default_graph()
 
     obs_dim = env.observation_space.shape
     act_dim = env.action_space.shape
     print("-- Observation space:", obs_dim, " Action space:", act_dim, "--")
 
     # Create some placeholders
-    obs_ph = tf.placeholder(shape=(None, obs_dim[0]), dtype=tf.float32, name="obs")
-    act_ph = tf.placeholder(shape=(None, act_dim[0]), dtype=tf.float32, name="act")
-    y_ph = tf.placeholder(shape=(None,), dtype=tf.float32, name="y")
+    obs_ph = tf.compat.v1.placeholder(shape=(None, obs_dim[0]), dtype=tf.float32, name="obs")
+    act_ph = tf.compat.v1.placeholder(shape=(None, act_dim[0]), dtype=tf.float32, name="act")
+    y_ph = tf.compat.v1.placeholder(shape=(None,), dtype=tf.float32, name="y")
 
     # Create an online deterministic actor and a double critic
-    with tf.variable_scope("online"):
+    with tf.compat.v1.variable_scope("online"):
         p_onl, qd1_onl, qa1_onl, _, qa2_onl = deterministic_actor_double_critic(
             obs_ph, act_ph, hidden_sizes, act_dim[0], np.max(env.action_space.high)
         )
 
     # and a target actor and double critic
-    with tf.variable_scope("target"):
+    with tf.compat.v1.variable_scope("target"):
         p_tar, _, qa1_tar, _, qa2_tar = deterministic_actor_double_critic(
             obs_ph, act_ph, hidden_sizes, act_dim[0], np.max(env.action_space.high)
         )
@@ -168,7 +66,7 @@ def TD3(
         """
         Retrieve all the variables in the scope 'scope'
         """
-        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope)
+        return tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope)
 
     # Copy all the online variables to the target networks i.e. target = online
     # Needed only at the beginning
@@ -197,11 +95,11 @@ def TD3(
     p_loss = -tf.reduce_mean(qd1_onl)
 
     # Optimize the critics
-    q1_opt = tf.train.AdamOptimizer(cr_lr).minimize(q1_loss)
-    q2_opt = tf.train.AdamOptimizer(cr_lr).minimize(q2_loss)
+    q1_opt = tf.compat.v1.train.AdamOptimizer(cr_lr).minimize(q1_loss)
+    q2_opt = tf.compat.v1.train.AdamOptimizer(cr_lr).minimize(q2_loss)
 
     # Optimize the actor
-    p_opt = tf.train.AdamOptimizer(ac_lr).minimize(
+    p_opt = tf.compat.v1.train.AdamOptimizer(ac_lr).minimize(
         p_loss, var_list=variables_in_scope("online/p_mlp")
     )
 
@@ -231,18 +129,18 @@ def TD3(
     tf.summary.scalar("loss/q1", q1_loss)
     tf.summary.scalar("loss/q2", q2_loss)
     tf.summary.scalar("loss/p", p_loss)
-    scalar_summary = tf.summary.merge_all()
+    scalar_summary = tf.compat.v1.summary.merge_all()
 
     hyp_str = "-aclr_" + str(ac_lr) + "-crlr_" + str(cr_lr) + "-tau_" + str(tau)
 
-    file_writer = tf.summary.FileWriter(
+    file_writer = tf.compat.v1.compat.v1.summary.FileWriter(
         "log_dir/" + env_name + "/TD3_" + clock_time + "_" + hyp_str,
-        tf.get_default_graph(),
+        tf.compat.v1.get_default_graph(),
     )
 
     # Create a session and initialize the variables
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+    sess = tf.compat.v1.Session()
+    sess.run(tf.compat.v1.global_variables_initializer())
     sess.run(init_target_op)
 
     # Some useful variables..
@@ -307,7 +205,7 @@ def TD3(
 
                 # Compute the target values
                 y_r = (
-                    np.array(mb_rew) + discount * (1 - np.array(mb_done)) * q_target_mb
+                        np.array(mb_rew) + discount * (1 - np.array(mb_done)) * q_target_mb
                 )
 
                 # Optimize the critics
@@ -333,7 +231,7 @@ def TD3(
 
                 # some 'mean' summaries to plot more smooth functions
                 if step_count % mean_summaries_steps == 0:
-                    summary = tf.Summary()
+                    summary = tf.compat.v1.Summary()
                     summary.value.add(
                         tag="loss/mean_q1", simple_value=np.mean(last_q1_update_loss)
                     )
@@ -358,7 +256,7 @@ def TD3(
         # Test the actor every 10 epochs
         if ep % 10 == 0:
             test_mn_rw, test_std_rw = test_agent(env_test, agent_op)
-            summary = tf.Summary()
+            summary = tf.compat.v1.Summary()
             summary.value.add(tag="test/reward", simple_value=test_mn_rw)
             file_writer.add_summary(summary, step_count)
             file_writer.flush()
@@ -386,19 +284,3 @@ def TD3(
     file_writer.close()
     env.close()
     env_test.close()
-
-
-if __name__ == "__main__":
-    TD3(
-        "BipedalWalker-v2",
-        hidden_sizes=[64, 64],
-        ac_lr=4e-4,
-        cr_lr=4e-4,
-        buffer_size=200000,
-        mean_summaries_steps=100,
-        batch_size=64,
-        min_buffer_size=10000,
-        tau=0.005,
-        policy_update_freq=2,
-        target_noise=0.1,
-    )
